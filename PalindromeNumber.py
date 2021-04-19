73a79fb4281eca0974e0a278fa04e363b44fd047
Kathiravan Natarajan <knatarajan@hra.com>
2021-04-19 13:35:19 -0400

    PalindromeNumber added

---
+++ PalindromeNumber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num = x 
+        rev_digit = 0 
+        if x < 0: 
+            return False 
+        else:
+            rev_digit = 0 
+            while num > 0:
+                rev_digit = rev_digit * 10 + num % 10 
+                num = num / 10         
+            if rev_digit == x: 
+                return True 
+        
